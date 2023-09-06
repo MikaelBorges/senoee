9acba33f280374fa9c3fa2d94326322af9118d15
Mikael B <borges.mikael@gmail.com>
2023-09-07 00:18:09 +0200

    change order of fetch functions

--- src/api/hackerNews.ts
+++ src/api/hackerNews.ts
@@ -1,23 +1,18 @@
 import { StoryType } from "../types/storyType";
 
+export const getTopStories = async () => {
+  const pretty = "";
+  const res = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?print=${pretty}`);
+  const result: number[] = await res.json();
+  return result;
+};
+
 export const getItemsByIds = async (arrayID: number[]) => {
   const pretty = "";
   const data: StoryType[] = await Promise.all(
     arrayID.map((id) =>
-      fetch(
-        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=${pretty}`
-      ).then((response) => response.json())
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=${pretty}`).then((response) => response.json())
     )
   );
-
   return data;
-};
-
-export const getTopStories = async () => {
-  const pretty = "";
-  const res = await fetch(
-    `https://hacker-news.firebaseio.com/v0/topstories.json?print=${pretty}`
-  );
-  const result: number[] = await res.json();
-  return result;
 };
